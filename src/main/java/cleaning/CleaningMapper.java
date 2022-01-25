aef2f5e3433a8385f112521a71860d368cc7c359
Antonio Agostino <antonio.agostino97@gmail.com>
2022-01-25 19:12:50 +0100

    Remove header cleaning part

--- src/main/java/cleaning/CleaningMapper.java
+++ src/main/java/cleaning/CleaningMapper.java
@@ -17,64 +17,62 @@ public class CleaningMapper extends Mapper<LongWritable, Text, Text, Text> {
             throws IOException, InterruptedException {
 
         String[] fields = value.toString().split(",");
+
+        // If one of the columns is absent the row is invalid
         if (fields.length == 9) {
 
-            // Header check: adding column names
-            if (fields[0].equals("Dates")) {
-                String newHeader = value + "TimeOfDay" + "Month" + "Year";
-                context.write(new Text(""), new Text(newHeader));
+            boolean nullRow = false;
+            String newAddress = "";
+            String timeOfDay = "";
+            String year = "";
+            String month = "";
+
+            for (String field : fields) {
+                // Null value removal
+                if (field.equals("")) {
+                    nullRow = true;
+                    break;
+                }
+
+                // Stopwords removal from address column
+                newAddress = fields[6].replace("av", "").
+                        replace("st", "").
+                        replace("of", "").
+                        replace("block", "")
+                        .replace("ln", "");
+
+                // Extracting time of day, month, and year from existing features
+                String date = fields[0].split(" ")[0];
+                String time = fields[0].split(" ")[1];
+
+                int hourOfDay = Integer.parseInt(time.split(":")[0]);
+                if (hourOfDay >= 5 && hourOfDay <= 11)
+                    timeOfDay = "Morning";
+                else if (hourOfDay >= 12 && hourOfDay <= 17)
+                    timeOfDay = "Afternoon";
+                else if (hourOfDay >= 18 && hourOfDay <= 21)
+                    timeOfDay = "Evening";
+                else
+                    timeOfDay = "Night";
+
+                year = date.split("-")[0];
+                month = Month.of(Integer.parseInt(date.split("-")[1])).
+                        getDisplayName(TextStyle.FULL_STANDALONE, new Locale("en-US"));
             }
-            else {
-                boolean nullRow = false;
-                String newAddress = "";
-                String timeOfDay = "";
-                String year = "";
-                String month = "";
-                for (String field : fields) {
-                    // Null value removal
-                    if (field.equals("")) {
-                        nullRow = true;
-                        break;
-                    }
-                    // Stopwords removal from address column
-                    newAddress = fields[6].replace("av", "").
-                            replace("st", "").
-                            replace("of", "").
-                            replace("block", "")
-                            .replace("ln", "");
 
-                    // Extracting time of day, month, and year from existing features
-                    String date = fields[0].split(" ")[0];
-                    String time = fields[0].split(" ")[1];
+            StringBuilder finalRow = new StringBuilder();
+            for (int i = 0; i < 6; i++) {
+                finalRow.append(fields[i]);
+            }
 
-                    int hourOfDay = Integer.parseInt(time.split(":")[0]);
-                    if (hourOfDay >= 5 && hourOfDay <= 11)
-                        timeOfDay = "Morning";
-                    else if (hourOfDay >= 12 && hourOfDay <= 17)
-                        timeOfDay = "Afternoon";
-                    else if (hourOfDay >= 18 && hourOfDay <= 21)
-                        timeOfDay = "Evening";
-                    else
-                        timeOfDay = "Night";
+            finalRow.append(newAddress).
+                    append(fields[6]).
+                    append(fields[7]).
+                    append(timeOfDay).
+                    append(month).append(year);
 
-                    year = date.split("-")[0];
-                    month = Month.of(Integer.parseInt(date.split("-")[1])).
-                            getDisplayName(TextStyle.FULL_STANDALONE, new Locale("en-US"));
-                }
-
-                StringBuilder finalRow = new StringBuilder();
-                for (int i = 0; i < 6; i++) {
-                    finalRow.append(fields[i]);
-                }
-                finalRow.append(newAddress).
-                        append(fields[6]).
-                        append(fields[7]).
-                        append(timeOfDay).
-                        append(month).append(year);
-
-                if (!nullRow) {
-                    context.write(new Text(""), new Text(finalRow.toString()));
-                }
+            if (!nullRow) {
+                context.write(new Text(""), new Text(finalRow.toString()));
             }
         }
     }
